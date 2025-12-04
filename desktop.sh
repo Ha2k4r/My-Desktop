ddbab13a139718902ca2da95d5c978279c3a0655
Ha2k4r <155183725+Ha2k4r@users.noreply.github.com>
2025-12-04 13:37:38 -0500

    desktop.sh improved everything
    Refactor script to use bash, add new functionality, and improve structure.

--- desktop.sh
+++ desktop.sh
@@ -1,57 +1,223 @@
-#!/bin/sh
-#
-#
-#	Author	-	Stolasbirb/Ha2k4r
-#	Repo	-	https://github.com/Ha2k4r/My-Desktop/
-#	Version -       0.1
-#
-#
+#! /bin/bash
 
-# Packages
 
-# Removing these may break individual apps but some of these are nice to haves that i always use for development and general use
-packages=(hyprpolkitagent krita swaync firefox hyprland zip unzip bc jq dosfstools cups pavucontrol arduino git bluez fish fastfetch nano waybar brightnessctl plymouth hyprlock kitty rofi dunst libnotify inotify-tools wget acpid swaybg
-	slurp grim playerctl gammastep kdeconnect iproute2 xdg-desktop-portal-hyprland libreoffice)
+creator_line1=' _____ _        _           ______ _      _     '
+creator_line2='/  ___| |      | |          | ___ (_)    | |    '
+creator_line3='\ `--.| |_ ___ | | __ _ ___ | |_/ /_ _ __| |__  '
+creator_line4=' `--. \ __/ _ \| |/ _` / __|| ___ \ | .__| |_ \ '
+creator_line5='/\__/ / || (_) | | (_| \__ \| |_/ / | |  | |_) |'
+creator_line6='\____/ \__\___/|_|\__,_|___/\____/|_|_|  |_.__/ '
+creator_line7='trans rights! yes, but my coding is a trans wrong
+'
 
-AURPackages=(vesktop alvr-bin)
 
-# Set some colors for output messages
-OK="$(tput setaf 2)[OK]$(tput sgr0)"
-ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
-NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
-INFO="$(tput setaf 4)[INFO]$(tput sgr0)"
-WARN="$(tput setaf 1)[WARN]$(tput sgr0)"
-CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
-MAGENTA="$(tput setaf 5)"
-ORANGE="$(tput setaf 214)"
-WARNING="$(tput setaf 1)"
-YELLOW="$(tput setaf 3)"
-GREEN="$(tput setaf 2)"
-BLUE="$(tput setaf 4)"
-SKY_BLUE="$(tput setaf 6)"
-RESET="$(tput sgr0)"
 
-# Check if running as root. If root, script will exit
-if [[ $EUID -eq 0 ]]; then
-    echo "${ERROR}  This script should ${WARNING}NOT${RESET} be executed as root!! Exiting......."
-    printf "\n%.0s" {1..2}
-    exit 1
+
+#ehh, makes it a lil easyer to reuse my template
+name_of_program='Pteradactyl'
+goodbye_message='Thank you for using my program, goodbye, and have a great day!'
+
+#defaults
+noconfirm=false
+nochecks=false
+nocolor=false
+quiet=false
+verbose=false
+
+#weird funky stuff
+aur_sandboxing=false
+
+
+### prebby colors (auto-disabled if piped)
+
+if [ -t 1 ]; then
+    # Green 
+    OK="$(tput setaf 2)[OK]$(tput sgr0)"
+
+    # Red
+    ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
+
+    # Yellow
+    NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+
+    # Blue 
+    INFO="$(tput setaf 4)[INFO]$(tput sgr0)"
+
+    # yellow
+    WARN="$(tput setaf 3)[WARN]$(tput sgr0)"
+
+    # Cyan for prompts
+    ACTION="$(tput setaf 6)[ACTION]$(tput sgr0)"
+
+    # Raw color 
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    MAGENTA="$(tput setaf 5)"
+    CYAN="$(tput setaf 6)"
+    WHITE="$(tput setaf 7)"  
+    C_RESET="$(tput sgr0)"
+else
+    # triggered when output is NOT a terminal 
+    OK="[OK]"
+    ERROR="[ERROR]"
+    NOTE="[NOTE]"
+    INFO="[INFO]"
+    WARN="[WARN]"
+    ACTION="[ACTION]"
+
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    MAGENTA=""
+    CYAN=""
+    WHITE=""
+    C_RESET=""
 fi
 
-# Check /etc/os-release to see if this is an Ubuntu or Debian based distro
-if grep -iq '^\(ID_LIKE\|ID\)=.*\(debian\|ubuntu\)' /etc/os-release >/dev/null 2>&1; then
-    echo "${ERROR} This script can only be run on a arch based distro, not Ubuntu or Debian. Exiting........"
-	exit 1
-fi
+log() {
+  local msg="$1"
+  local force="$2"
 
-clear
+  if [[ "$quiet" == true && "$force" != "force" ]]; then
+    return 0
+  fi  
+  
+  if [[ "$nocolor" == true ]]; then
+    #strip all forms of color
+    msg=$(printf '%s' "$msg" \
+         | sed -r 's/\x1B\[[0-9;]*[mK]//g')
+  fi 
 
+  if [[ "$verbose" == true ]]; then
+    local timestamp="[$(date +"%Y-%m-%d %H:%M:%S")] "
+  else
+    local timestamp=""
+  fi
+
+  echo "${timestamp}${msg}${C_RESET}"
+}
+
+error() {
+  #ignores silent argument parse and prints in a nicely formated way
+  log "${RED}[ERROR] $*" force
+}
+
+#functions area
+parse_arguments() {
+  while test $# -gt 0; do
+    case "$1" in
+    --help | -h | help)
+      # Return this message and exit
+      echo -e "This script installs $name_of_program a open-source game management panel to your device.
+usage: PROGRAM {Flags..}
+
+Valid Flags:
+-h,  --help,    shows this menu and exits
+--noconfirm,    Never prompts the user and assumes defaults
+--nochecks,     Does not perform system safety checks and runs blindly
+-nc, --nocolor, Does not show colored output :(
+-q, --quiet,    Silences non required information
+-v, --verbose,  Output has timestamps"
+      exit 0 
+      ;;
+    --noconfirm)
+	noconfirm=true
+	shift
+      ;;
+    --nochecks)
+        nochecks=true
+	shift
+      ;; 
+    --nc | --nocolor | --nocolors)
+	nocolor=true
+	shift
+      ;;
+    -q | --quiet)
+    	quiet=true
+	shift
+      ;;
+    -v | --verbose)
+	verbose=true
+	shift
+      ;;
+    *)
+      error "unexpected argument : '${*}' found
+usage PROGRAM [Flags]
+for more information, try '--help'"
+      exit 1
+      ;;
+    esac
+  done
+}
+
+check_root() {
+  if [[ "$nochecks" == false ]]; then
+    if [[ !"$(id -u)" -ne 0 ]]; then
+      error "This script cannot be run as root. try again without 'sudo' any user that is not 'su' "
+      exit 1
+    fi 
+  fi
+}
+
+check_operating_system() {
+  if [ "$nochecks" = false ]; then
+    if !(grep -q "Arch Linux" /etc/os-release); then
+      error "This script can only be used on Arch Linux."
+      exit 1
+    fi
+  fi
+}
+install_yay(){
+  sudo pacman -S --needed --noconfirm git base-devel
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  pushd /tmp/yay
+  makepkg -si --noconfirm
+  popd
+}
+check_yay_installed() {
+  if [ "$nochecks" = false ]; then
+    if ! command -v yay &> /dev/null; then
+      if [ "$noconfirm" = false ]; then
+        if prompt_user "yay is not installed, and is required by this program, would you like to install it?"; then
+	install_yay
+        else
+	exit 0
+        fi 
+      else
+        log "yay not found – installing..."
+	install_yay
+      fi
+    fi
+  fi
+}
+
+update_system() {
+  log "Updating the system..."
+  pacman -Syu --noconfirm --needed
+}
+prompt_user() {
+  local answer
+  local prompt="$1"
+  while true; do
+    read -r -p "$prompt (Y/n) " answer
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+      return 0 #true
+    elif [[ "$answer" =~ ^[Nn]$ ]]; then
+      return 1 #false
+    else
+      error "Invalid input, please type Y or N."
+    fi
+  done
+}
 install_package() {
   if ! pacman -Qi "$1" &>/dev/null; then
-    echo "${GREEN}Installing : ${SKY_BLUE} $1 ${RESET}. . ."
+    log "${GREEN}Installing : ${BLUE}$1. . ."
     sudo pacman -S --noconfirm "$1"
   else
-    echo "${NOTE}$1 is already installed. ${RESET}"
+    log "${NOTE}$1 is already installed."
   fi
 }
 
@@ -63,74 +229,55 @@ install_aur_package() {
     echo "${NOTE}$1 is already installed.${RESET}"
   fi
 }
+implementation() {
+  #Things to install 
+  packages=(swww hyprpolkitagent krita swaync firefox hyprland zip unzip bc jq dosfstools cups pavucontrol arduino git bluez fish fastfetch nano waybar brightnessctl plymouth hyprlock kitty rofi dunst libnotify inotify-tools wget acpid swaybg slurp grim playerctl gammastep kdeconnect iproute2 xdg-desktop-portal-hyprland libreoffice) 
 
-Install_YAY() {
-  if ! command -v yay &>/dev/null; then
-    log "${GREEN}Installing yay (AUR helper)..."
-    sudo pacman -S --noconfirm --needed base-devel git
+  AURPackages=(python-pywal vesktop alvr-bin)
 
-    tempdir=$(mktemp -d)
-    git clone https://aur.archlinux.org/yay.git "$tempdir/yay"
-    pushd "$tempdir/yay" >/dev/null
-    makepkg -si --noconfirm
-    popd >/dev/null
-    rm -rf "$tempdir"
+  for pkg in "${packages[@]}"; do
+    install_package "$pkg"
+  done
 
-    echo "${GREEN}yay installed successfully.${RESET}"
-  else
-    echo "${NOTE}yay is already installed.${RESET}"
-  fi
-}
+  for pkg in "${AURPackages[@]}"; do
+    install_aur_package "$pkg"
+  done
 
 
-#
-# Implementation
-#
-
-for pkg in "${packages[@]}"; do
-  install_package "$pkg"
-done
-
-Install_YAY
-for pkg in "${AURPackages[@]}"; do
-  install_aur_package "$pkg"
-done
-
-#
-# Configuration
-#
-
-
-for package in "${packages[@]}"; do
+  for package in "${packages[@]}"; do
   case "$package" in
+  kitty)
+    #install Fonts 
+    curl -L -o /tmp/tmpfont.zip 'https://www.dropbox.com/scl/fi/fcjwlalz1zq19a05tdw0f/elenapan-dotfiles-fonts.zip?dl=0&e=1&rlkey=uljkjoyi5qipi6hc9ju9ibk4o&st=zwqrvroc'
+    unzip -d ~/.local/share/fonts tmp/tmpfont.zip
+    fc-cache -v
+    ;;
   bluez)
     if ! systemctl is-enabled --quiet bluetooth.service; then
-      echo "${GREEN}Enabling Bluetooth service...${RESET}"
+      echo "${GREEN}Enabling Bluetooth service..."
       sudo systemctl enable --now bluetooth.service
     else
-      log "${YELLOW}Bluetooth service already enabled.${RESET}"
+      log "${YELLOW}Bluetooth service already enabled."
     fi
     ;;
   fish)
     if [[ $SHELL != "/usr/bin/fish" ]]; then
-      echo "${GREEN}Changing shell to Fish...${RESET}"
+      log "${GREEN}Changing shell to Fish..."
       sudo chsh -s /usr/bin/fish
     else
-      echo "${NOTE}Shell is already Fish${RESET}"
+      log "${NOTE}Shell is already Fish..."
     fi
     ;;
   arduino-ide)
-    echo "${GREEN}Configuring arduino-ide..${RESET}"
+    log "${GREEN}Configuring arduino-ide.."
     sudo usermod -a -G uucp $USER
     ;;
   cups)
-    echo "${GREEN}Configuring cups..${RESET}"
+    log "${GREEN}Configuring cups.."
     sudo systemctl enable --now cups
     ;;
   esac
 done
-
-
 
 for package in "${AURPackages[@]}"; do
   case "$package" in
@@ -139,50 +286,61 @@ for package in "${AURPackages[@]}"; do
   esac
 done
 
-
-# Fonts
-
-curl -L -o /tmp/tmpfont.zip 'https://www.dropbox.com/scl/fi/fcjwlalz1zq19a05tdw0f/elenapan-dotfiles-fonts.zip?dl=0&e=1&rlkey=uljkjoyi5qipi6hc9ju9ibk4o&st=zwqrvroc'
-
-unzip -d ~/.local/share/fonts tmp/tmpfont.zip
-
-fc-cache -v
-
-# Installing the dotfiles
-
-#git clone https://github.com/elenapan/dotfiles
-
-#cd dotfiles
-
-#cp -r config/{sway,eww,dunst,fontconfig,kitty,rofi} ~/.config
-
 # Wallpaper
 #mkdir Pictures Pictures/wallpapers/
 #curl --output ~/Pictures/wallpapers/ https://i.redd.it/zz55pru3ee0f1.png
 
 
-
-# Hyprland Dotfiles
-
+#Hyprland Dotfiles
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 rm -rf "$HOME/.config/hypr/"
+
 cp -rf "$SCRIPT_DIR"/src/hypr* "$HOME/.config/"
-
-#sudo cp -f "$SCRIPT_DIR"/src/fish/weatherService/weather-fetch.service "/etc/systemd/system/"
-#sudo cp -f "$SCRIPT_DIR"/src/fish/weatherService/weather-fetch.timer "/etc/systemd/system/"
-#sudo cp -f "$SCRIPT_DIR"/src/fish/WeatherFetchBIN/fetch_weather.sh "/usr/bin/"
-
-#sudo chmod +x "/usr/bin/fetch_weather.sh"
-
-#log "${GREEN}Finishing up now.."
-
-#sudo systemctl daemon-reexec && sudo systemctl daemon-reload
-#sudo systemctl enable --now weather-fetch.timer
-
-#cp -rf "$SCRIPT_DIR"/src/fish/* "$HOME/.config/fish/"
 
 sudo cp -f "$SCRIPT_DIR"/src/waybar/config.jsonc "/etc/xdg/waybar/"
 
 sudo cp -f "$SCRIPT_DIR"/src/waybar/style.css "/etc/xdg/waybar/"
-#cp -rf "$SCRIPT_DIR"/src/kitty/kitty.conf ~/.config/kitty/
+cp -rf "$SCRIPT_DIR"/src/kitty/kitty.conf ~/.config/kitty/
+
+#make scripts executable
+chmod +x ~/.config/hypr/scripts/*
+}
+
+
+
+modify_system_dialog() {
+  if !($noconfirm) ; then 
+    if prompt_user "This script will modify your System, and install $name_of_program. 
+Please enter 'N' to exit or 'Y' to continue. "; then
+      return 0
+    else
+      error "User denied action. Exiting."
+      exit 1 
+    fi 
+  else
+    return 0  
+  fi 
+}
+print_creator(){
+  log "
+${BLUE}$creator_line1
+${MAGENTA}$creator_line2
+${WHITE}$creator_line3
+${WHITE}$creator_line4
+${MAGENTA}$creator_line5
+${BLUE}$creator_line6
+${GREEN}$creator_line7"
+}
+
+main() {
+  parse_arguments "$@"
+  check_root
+  check_operating_system
+  modify_system_dialog
+  implementation
+  print_creator
+
+  log "${GREEN}$goodbye_message"
+}
+main "$@"
